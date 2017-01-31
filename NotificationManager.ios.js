4bf838f0c9058f2850ac0be3c723f7874c169537
Alessandro Giannini <alessandro@develer.com>
2017-01-31 13:00:32 +0100

    Provides local notification for ios apps
    Referred issue: https://github.com/develersrl/app-pranzi/issues/6

--- NotificationManager.ios.js
+++ NotificationManager.ios.js
@@ -1,10 +1,41 @@
+import PushNotification from 'react-native-push-notification';
+
+import I18n from 'react-native-i18n';
+
 export default class NotificationManager {
 
-  static clearNotifications() {}
+  static clearNotifications() {
+    PushNotification.cancelAllLocalNotifications();
+  }
 
-  static setNotification(date) {}
+  static setNotification(date) {
+    console.log('Setting notification for date: ');
+    console.log(date);
+    PushNotification.localNotificationSchedule({
+      title: 'App Pranzi',
+      message: I18n.t('notificationText'),
+      repeatType: 'week',
+      date: date,
+    });
+  }
 
-  static setNotifications(...dates) {}
+  static scheduleNotificationInSecs(secs) {
+    console.log(`Scheduling local notification in ${secs} seconds`);
+    PushNotification.localNotificationSchedule({
+      title: 'App Pranzi',
+      message: I18n.t('notificationText'),
+      date: new Date(Date.now() + secs * 1000),
+    });
+  }
 
-  static resetNotifications(...date) {}
+  static setNotifications(dates) {
+    dates.forEach((date) => {
+      NotificationManager.setNotification(date);
+    });
+  }
+
+  static resetNotifications(dates) {
+    NotificationManager.clearNotifications();
+    NotificationManager.setNotifications(dates);
+  }
 }
